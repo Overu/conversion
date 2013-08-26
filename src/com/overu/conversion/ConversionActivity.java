8f5a8229cba2017aa0be8942250f3b8143c99bf2
fengjian <fengjian@goodow.com>
2013-08-26 21:12:05 +0800

    init conversion

---
+++ src/com/overu/conversion/ConversionActivity.java
@@ -0,0 +1,22 @@
+package com.overu.conversion;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ConversionActivity extends Activity {
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.conversion, menu);
+    return true;
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_conversion);
+  }
+
+}
